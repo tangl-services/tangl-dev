6ab14013099499ae3288397181c6ae65a213a42d
Aleksei Lobanov <luxliber@gmail.com>
2022-11-14 22:53:08 +0500

    demo app update

--- tangl-demo-basic/src/index.js
+++ tangl-demo-basic/src/index.js
@@ -23,6 +23,25 @@ async function fetchToken() {
 	}
 }
 
+async function fetchCompanyInfo() {
+	const response = await window.axios.post(TANGL_AUTH_SERVER + "/connect/token", {
+			 client_id: "e35e3f8b-8197-5b4d-8249-3a077cfedc50",
+			 client_secret: "ff7eb513-db87-b66d-f743-3a077cfedc51",
+			 grant_type: "password",
+			 username: "devdemo@tangl.cloud",
+			 password: "Devdemo1!",
+		 },
+		 {
+			 headers: {
+				 'Content-Type': 'application/x-www-form-urlencoded'
+			 },
+		 })
+
+	if (response?.data?.access_token) {
+		TANGL_TOKEN = response.data.access_token;
+		console.info("Acces token is fetched: ", response.data.access_token)
+	}
+}
 
 await fetchToken()
 
