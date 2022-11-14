00c24a52b819ae7c4e8de6fe4b12885e64b76c44
Aleksei Lobanov <luxliber@gmail.com>
2022-11-14 16:44:18 +0500

    update demo app

--- tangl-demo-basic/src/index.js
+++ tangl-demo-basic/src/index.js
@@ -1,12 +1,43 @@
 ﻿import {OrbitControllerExtension, SceneManager, viewerStore} from "tangl-viewer"
 
-const sceneMan = new SceneManager()
-const renderMan = viewerStore.createRenderManager("default", sceneMan)
+const TANGL_AUTH_SERVER = "https://auth.st.tangl.cloud"
+var TANGL_TOKEN = undefined;
 
-renderMan.init("viewer")
-renderMan.extMan.addExtension(OrbitControllerExtension)
-renderMan.extMan.selectControllerExtension("orbit")
+async function fetchToken() {
+	const response = await window.axios.post(TANGL_AUTH_SERVER + "/connect/token", {
+			 client_id: "e35e3f8b-8197-5b4d-8249-3a077cfedc50",
+			 client_secret: "ff7eb513-db87-b66d-f743-3a077cfedc51",
+			 grant_type: "password",
+			 username: "devdemo@tangl.cloud",
+			 password: "Devdemo1!",
+		 },
+		 {
+			 headers: {
+				 'Content-Type': 'application/x-www-form-urlencoded'
+			 },
+		 })
 
-sceneMan.onAllLoaded(() => {
-	renderMan.zoomCameraToSelection()
+	if (response?.data?.access_token) {
+		TANGL_TOKEN = response.data.access_token;
+		console.info("Acces token is fetched: ", response.data.access_token)
+	}
+}
+
+
+await fetchToken()
+
+//create main managers
+const sceneManager = new SceneManager()
+const renderManager = viewerStore.createRenderManager("default", sceneManager)
+
+//init RenderManager with "viewer" DOM element
+renderManager.init("viewer")
+//add orbit controller extension for basic camera transformation
+renderManager.extMan.addExtension(OrbitControllerExtension)
+//select orbit extension as default
+renderManager.extMan.selectControllerExtension("orbit")
+
+//load scene from bucket via bucket GUID and zoom camera after load process
+sceneManager.onAllLoaded(() => {
+	renderManager.zoomCameraToSelection()
 }).load("0491523c-3e44-f638-93aa-3a077e411e29")
