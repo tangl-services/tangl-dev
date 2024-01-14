37d6601d5319276aecf7e92b4ac1df47b7aa48e2
Alexey Lobanov <luxliber@gmail.com>
2024-01-14 13:37:44 +0500

    fix: remove obsolete demo app

--- tangl-demo-advanced/src/managers.ts
+++
@@ -1,7 +0,0 @@
-﻿import {MetaManager, SceneManager, viewerStore} from "tangl-viewer";
-
-export const sceneManager = new SceneManager()
-export const renderManager = viewerStore.createRenderManager("default", sceneManager)!
-export const metaManager = new MetaManager()
-metaManager.useCache = true;
- 
